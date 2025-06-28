035dd6adf6020165f6e048370f69cb5544336957
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-06-28 18:27:07 +0100

    Add Author and Hero sections

---
+++ src/lib/components/AuthorSection.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import authorPic from '$assets/authorPic.jpeg';
+</script>
+
+<section class="landing-page-section">
+	<h2 class="mb-l">About the Author</h2>
+	<div class="author-container">
+		<div class="author-text">
+			<p class="mb-s">
+				Hi, I'm Kenechukwu Ajufo, a digital nomad who has lived in Spain for over 5 years. I created
+				this ebook to help others avoid the common pitfalls I faced when moving abroad.
+			</p>
+			<p>
+				My journey has been filled with challenges and triumphs, and I want to share my experiences
+				to make your move smoother and more enjoyable.
+			</p>
+		</div>
+		<img src={authorPic} alt="Author Kenechukwu Ajufo" />
+	</div>
+</section>
+
+<style>
+	.author-container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.author-container img {
+		width: 40%;
+		border: 2px solid black;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+	}
+
+	.author-text {
+		width: 55%;
+	}
+</style>
---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { Button } from '$components';
+	import type { Snippet } from 'svelte';
+	import bookCover from '$assets/book_cover.png';
+	import phoneCover from '$assets/phone_cover.png';
+	let { children }: { children?: Snippet } = $props();
+</script>
+
+<section class="hero">
+	<div class="hero-showcase text-center bold">
+		<h1 class="purple">MOVE TO</h1>
+		<h1 class="purple mb-xs">SPAIN</h1>
+		<h4 class="italic semi-bold">There is no need to make the same mistakes I did.</h4>
+		<img src={bookCover} alt="Book Cover" class="book-cover" />
+		<img src={phoneCover} alt="Phone Cover" class="phone-cover" />
+	</div>
+	<div class="hero-text white text-center">
+		{#if children}
+			{@render children()}
+		{:else}
+			<h1>THIS EBOOK WILL</h1>
+
+			<h1>SAVE YOU MONEY,</h1>
+			<h1 class="mb-m">TIME & NERVES.</h1>
+			<p class="light-grey mb-l">
+				You will learn how to move to Spain, what to do before you leave, and how to settle in
+				easily.
+			</p>
+			<Button>Purchase e-book for €15</Button>
+		{/if}
+	</div>
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		height: 100vh;
+		overflow-y: hidden;
+	}
+
+	.hero-showcase {
+		position: relative;
+		width: 50%;
+		padding: 100px 16px 0 16px;
+		background: linear-gradient(135deg, #e2cbff, #e2cbff 25%, #deeaff 50%, #ece5ff 75%, #927bce);
+	}
+
+	.hero-text {
+		padding: 100px 5%;
+		width: 50%;
+		background-color: black;
+	}
+
+	.book-cover {
+		position: absolute;
+		right: 2%;
+		bottom: 0;
+		width: 65%;
+		transform: translateY(30%);
+	}
+
+	.phone-cover {
+		position: absolute;
+		width: 35%;
+		bottom: 0;
+		right: 52%;
+		transform: translateY(25%);
+	}
+</style>
