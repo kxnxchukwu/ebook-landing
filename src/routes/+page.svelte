f9f961993b0d7aabb8d11a760f2729a6ac3f745f
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2025-06-28 18:30:20 +0100

    adding svelte generated files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { HeroSection, ChapterPreview, AuthorSection, FaqSection } from '$components';
+</script>
+
+<HeroSection />
+<ChapterPreview />
+<AuthorSection />
+<FaqSection />
